d57e2b80a803cdeabe0fc796dcdd6b1ece451865
bodomilan <bodomilan4@gmail.com>
2024-03-25 18:56:05 +0100

    model i skybox

---
+++ cmake-build-debug/CMakeFiles/project_base.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/bodo/Desktop/grafika_mi21173/project_base/project_base"
+  "/home/bodo/Desktop/grafika_mi21173/project_base/project_base.pdb"
+  "CMakeFiles/project_base.dir/src/main.cpp.o"
+  "CMakeFiles/project_base.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project_base.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/configuration/root_directory.h
@@ -0,0 +1,1 @@
+const char * logl_root = "/home/bodo/Desktop/grafika_mi21173/project_base";
